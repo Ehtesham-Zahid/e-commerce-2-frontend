499471cc0e784954b81d3aa402765ffbe193b2d8
Malik-Ehtesham <shamimalick321@gmail.com>
2024-05-31 00:45:30 +0500

    product and cart pages done

--- src/components/SingleProductSection/SingleProductSection.jsx
+++ src/components/SingleProductSection/SingleProductSection.jsx
@@ -8,8 +8,18 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "@/shadcn-components/ui/avatar";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/shadcn-components/ui/accordion";
+
 import { Button } from "@/shadcn-components/ui/button";
 import { Separator } from "@/shadcn-components/ui/separator";
+import RecyclingIcon from "@mui/icons-material/Recycling";
+import DescriptionIcon from "@mui/icons-material/Description";
+import WarningIcon from "@mui/icons-material/Warning";
 
 const SingleProductSection = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -34,55 +44,128 @@ const SingleProductSection = () => {
       <div className={`${scrolled ? "fixed top-0 z-10" : ""}`}>
         <Header />
       </div>
-
       <div className="flex justify-center">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4  w-11/12 xl:w-5/6 2xl:w-3/4 mt-10">
-          <ProductImagesSlider />
-          <div className="col-span-1  py-4 ">
-            <p className="text-2xl font-bold ">Cloud Feel Muscle Tee</p>
-            <p className=" text-sm text-gray-500 mb-4">MEN</p>
-            <p className="text-xl font-bold mb-10">Rs.2600</p>
-            <Separator className="mb-12" />
-            <p className=" mb-2 text-lg font-medium text-gray-500 ">
-              Color - White
-              {/* <span className="   py-2 px-4  rounded-full     bg-black"></span> */}
-            </p>
-            <p className="text-lg font-medium text-gray-500">Colors:</p>
-            <div className="flex items-center justify-between w-64 mb-8">
-              <Avatar className="bg-red-600  " />
-              <Avatar className="bg-black  " />
-              <Avatar className="bg-gray-500  " />
-              <Avatar className="bg-red-600  " />
-              <Avatar className="bg-red-600  " />
-            </div>
-            <p className="text-lg font-medium text-gray-500 ">Size</p>
-            {/* <div className="flex justify"> */}
-            <div className="flex justify-between  flex-wrap overflow-auto gap-y-5 gap-x-2 items-center mb-12 ">
-              <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
-                XS
+        <div className="flex justify-center flex-col    w-11/12 xl:w-5/6 2xl:w-3/4">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4  mt-10">
+            <ProductImagesSlider />
+            <div className="col-span-1  py-4 ">
+              <p className="text-2xl font-bold ">Cloud Feel Muscle Tee</p>
+              <p className=" text-sm text-gray-500 mb-4">MEN</p>
+              <p className="text-xl font-bold mb-10">Rs.2600</p>
+              <Separator className="mb-12" />
+              <p className=" mb-2 text-lg font-medium text-gray-500 ">
+                Color - White
+                {/* <span className="   py-2 px-4  rounded-full     bg-black"></span> */}
               </p>
-              <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
-                S
-              </p>{" "}
-              <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
-                M
-              </p>{" "}
-              <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
-                L
-              </p>
-              <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
-                XL
-              </p>{" "}
-              <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
-                XXL
-              </p>
-            </div>
-            <Separator className="mb-10" />
-            {/* <p className="rounded-sm  border  px-6 py-4">S</p>{" "}
+              <p className="text-lg font-medium text-gray-500">Colors:</p>
+              <div className="flex items-center justify-between w-64 mb-8">
+                <Avatar className="bg-red-600  " />
+                <Avatar className="bg-black  " />
+                <Avatar className="bg-gray-500  " />
+                <Avatar className="bg-red-600  " />
+                <Avatar className="bg-red-600  " />
+              </div>
+              <p className="text-lg font-medium text-gray-500 ">Size</p>
+              {/* <div className="flex justify"> */}
+              <div className="flex justify-between  flex-wrap overflow-auto gap-y-5 gap-x-2 items-center mb-12 ">
+                <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
+                  XS
+                </p>
+                <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
+                  S
+                </p>{" "}
+                <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
+                  M
+                </p>{" "}
+                <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
+                  L
+                </p>
+                <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
+                  XL
+                </p>{" "}
+                <p className="rounded-sm  border-2 border-gray-300 text-gray-400   text-center py-1.5 w-20 ">
+                  XXL
+                </p>
+              </div>
+              <Separator className="mb-10" />
+              {/* <p className="rounded-sm  border  px-6 py-4">S</p>{" "}
             <p className="rounded-sm  border  px-6 py-4">S</p>{" "}
             <p className="rounded-sm  border  px-6 py-4">S</p> */}
-            {/* </div> */}
-            <Button className="w-full 2xl:mt-10 py-5">ADD TO CART</Button>
+              {/* </div> */}
+              <Button className="w-full 2xl:mt-10 py-5">ADD TO CART</Button>
+            </div>
+          </div>
+          <div className="my-10">
+            <Accordion
+              type="single"
+              collapsible
+              className="border-y border-gray-400  "
+            >
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="text-lg text-gray-700">
+                  <p className="flex items-center">
+                    <DescriptionIcon className="me-3" fontSize="small" />{" "}
+                    DESCRIPTION
+                  </p>
+                </AccordionTrigger>
+                <AccordionContent className="text-medium tracking-wider leading-7">
+                  Yes. It adheres to the WAI-ARIA design pattern. Yes. It
+                  adheres to the WAI-ARIA design pattern. Yes. It adheres to the
+                  WAI-ARIA design pattern. Yes. It adheres to the WAI-ARIA
+                  design pattern. Yes. It adheres to the WAI-ARIA design
+                  pattern. Yes. It adheres to the WAI-ARIA design pattern. Yes.
+                  It adheres to the WAI-ARIA design pattern. Yes. It adheres to
+                  the WAI-ARIA design pattern. Yes. It adheres to the WAI-ARIA
+                  design pattern. Yes. It adheres to the WAI-ARIA design
+                  pattern. Yes. It adheres to the WAI-ARIA design pattern. Yes.
+                  It adheres to the WAI-ARIA design pattern. Yes. It adheres to
+                  the WAI-ARIA design pattern. Yes. It adheres to the WAI-ARIA
+                  design pattern. Yes. It adheres to the WAI-ARIA design
+                  pattern. Yes. It adheres to the WAI-ARIA design pattern. Yes.
+                  It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+            <Accordion
+              type="single"
+              collapsible
+              className="border-b border-gray-400"
+            >
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="text-lg text-gray-700 ">
+                  <p className="flex items-center">
+                    <RecyclingIcon className="me-3" fontSize="small" />{" "}
+                    COMPOSITION
+                  </p>
+                </AccordionTrigger>
+                <AccordionContent className="text-lg">
+                  Cotton 100%
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>{" "}
+            <Accordion
+              type="single"
+              collapsible
+              className="border-b border-gray-400"
+            >
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="text-lg text-gray-700">
+                  <p className="flex items-center">
+                    <WarningIcon className="me-3" fontSize="small" /> MATERIAL &
+                    CARE
+                  </p>
+                </AccordionTrigger>
+                <AccordionContent className="tracking-wide leading-6">
+                  <p>o Do not bleach</p>
+                  <p>o Do not dry clean</p>
+                  <p>o Low iron if needed</p>
+                  <p>o Machine wash cold</p>
+                  <p>o Do not use fabric softener</p>
+                  <p>o Use mild detergent only</p>
+                  <p>o Wash with like colors</p>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
         </div>
       </div>
