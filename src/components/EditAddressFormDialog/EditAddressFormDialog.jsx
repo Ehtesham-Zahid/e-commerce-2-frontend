8d06628031dedd3ed47fd9355ae5366320e00958
Malik-Ehtesham <shamimalick321@gmail.com>
2024-06-17 05:08:00 +0500

    Checkout Form Completed

--- src/components/EditAddressFormDialog/EditAddressFormDialog.jsx
+++ src/components/EditAddressFormDialog/EditAddressFormDialog.jsx
@@ -51,14 +51,14 @@ const EditAddressFormDialog = (props) => {
     <>
       <button
         onClick={() => {
-          document.getElementById("my_modal_2").showModal();
+          document.getElementById(props.id).showModal();
         }}
         className="font-semibold cool-link cursor-pointer hover:text-blue-500"
       >
         Edit
       </button>
 
-      <dialog id="my_modal_2" className="modal">
+      <dialog id={props.id} className="modal">
         <div className="modal-box">
           <form method="dialog  ">
             {/* if there is a button in form, it will close the modal */}
